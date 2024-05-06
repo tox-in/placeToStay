efb0a1394c46be70afbaa82c3b9e2ad3e3004916
tox-in <habimanat78@gmail.com>
2024-05-06 09:24:24 +0200

    added a reducer function

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import { AppBar, Box, Container, IconButton, Toolbar, Typography, Button } from '@mui/material';
+import { Menu, Lock } from '@mui/icons-material';
 
 const NavBar = () => {
   return (
-    <div>
-      
-    </div>
+    <AppBar>
+        <Container maxWidth='lg'>
+            <Toolbar disableGutters>
+                <Box sx={{mr:1}}>
+                    <IconButton size='large' color='inherit'>
+                        <Menu />
+                    </IconButton>
+                </Box>
+                <Typography
+                variant='h6'
+                component='h1'
+                noWrap
+                sx={{flexGrow:1, display:{xs:'none', md:'flex'}}}
+                >
+                    You Are Welcome
+                </Typography>
+                <Typography
+                variant='h6'
+                component='h1'
+                noWrap
+                sx={{flexGrow:1, display:{xs:'flex', md:'none'}}}
+                >
+                    YAW
+                </Typography>
+                <Button
+                color='inherit'
+                startIcon={<Lock/>}
+                >
+                    Login
+                </Button>
+            </Toolbar>
+        </Container>
+    </AppBar>
   )
 }
 
