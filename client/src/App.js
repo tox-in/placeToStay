08c26d85e965fea578ddffd1d40a94a64767bc4a
tox-in <habimanat78@gmail.com>
2024-05-07 21:03:48 +0200

    new changes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Login from './components/user/Login'
+import Notification from './components/Notification'
 
 const App = () => {
   return (
+    <>
+   <Notification />
+   <Login />
    <NavBar />
+   </>
   )
 }
 
