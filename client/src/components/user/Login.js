907e3f61f25381d44b54737672b651ec407f6cd4
tox-in <habimanat78@gmail.com>
2024-05-07 21:02:13 +0200

    changes in Login and PasswordField and changed from useRef to onChange

--- client/src/components/user/Login.js
+++ client/src/components/user/Login.js
@@ -1,113 +1,144 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, TextField } from '@mui/material'
-import React, { useEffect, useRef, useState } from 'react'
-import { useValue } from '../../context/contextProvider'
-import { Close, Send } from '@mui/icons-material'
-import PasswordField from './PasswordField'
-import GoogleOneTapLogin from './GoogleOneTapLogin'
+import React, { useEffect, useRef, useState } from 'react';
+import { useValue } from '../../context/contextProvider';
+import { Close, Send } from '@mui/icons-material';
+import PasswordField from './PasswordField';
+import GoogleOneTapLogin from './GoogleOneTapLogin';
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    IconButton,
+    TextField,
+} from '@mui/material';
 
 const Login = () => {
-    const {state:{openLogin}, dispatch} = useValue()
-    const [title, setTitle] = useState('Login')
-    const [isRegister, setIsRegister] = useState(false)
-    const nameRef = useRef()
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const confirmPasswordRef = useRef()
+    const { state: { openLogin }, dispatch } = useValue();
+    const [title, setTitle] = useState('Login');
+    const [isRegister, setIsRegister] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const nameRef = useRef();
+    const emailRef = useRef();
 
     const handleClose = () => {
-        dispatch({type:'CLOSE_LOGIN'})
-    }
+        dispatch({ type: 'CLOSE_LOGIN' });
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const password = passwordRef?.current?.value;
-        const confirmPassword = confirmPasswordRef?.current?.value;
+
+        console.log('Name value:', name);
+        console.log('Email value:', email);
+        console.log('Password value:', password);
+        console.log('Confirm password value:', confirmPassword);
+
         if (password !== confirmPassword) {
             dispatch({
                 type: 'UPDATE_ALERT',
-                payload: { open: true, severity: 'error', message: 'Passwords don\'t match' },
+                payload: { open: true, severity: 'error', message: 'Passwords don\'t match!' },
             });
+            console.log("passwords mismatch!")
             return;
         }
-        // Perform other form submission tasks
     };
 
-    useEffect(()=>{
-        isRegister ? setTitle('Register') : setTitle('Login');
-    }, [isRegister])
+    useEffect(() => {
+        if (isRegister) {
+            setTitle('Register');
+        } else {
+            setTitle('Login');
+        }
+    }, [isRegister]);
 
-  return (
-    <Dialog
-    open={openLogin}
-    onClose={handleClose}
-    >
-        <DialogTitle>
-            {title}
-            <IconButton
-            sx={{
-                position:'absolute',
-                top:8,
-                right:8,
-                color:(theme)=>theme.palette.grey[500]
-            }}
-            onClick={handleClose}
-            >
-                <Close />
-            </IconButton>
-        </DialogTitle>
-        <form onSubmit={handleSubmit}>
-            <DialogContent dividers>
-                <DialogContentText>
-                    Please fill your information in the fields below:
-                </DialogContentText>
-                {isRegister &&
-                <TextField
-                autoFocus
-                margin='normal'
-                variant='standard'
-                id='name'
-                label='Name'
-                type='text'
-                fullWidth
-                inputRef={nameRef}
-                inputProps={{minLength:2}}
-                required
-                />
-                }
-                <TextField
-                autoFocus={!isRegister}
-                margin='normal'
-                variant='standard'
-                id='email'
-                label='Email'
-                type='text'
-                fullWidth
-                inputRef={emailRef}
-                required
-                />
-                <PasswordField {...{passwordRef}} />
-                {
-                    isRegister && 
-                    <PasswordField inputRef={confirmPasswordRef} id='confirmPassword' label='Confirm Password' />
-                }
-            </DialogContent>
-            <DialogActions>
-                <Button type='submit' variant='contained' endIcon={<Send />}>
-                    Submit
+    return (
+        <Dialog open={openLogin} onClose={handleClose}>
+            <DialogTitle>
+                {title}
+                <IconButton
+                    sx={{
+                        position: 'absolute',
+                        top: 8,
+                        right: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                    onClick={handleClose}
+                >
+                    <Close />
+                </IconButton>
+            </DialogTitle>
+            <form onSubmit={handleSubmit}>
+                <DialogContent dividers>
+                    <DialogContentText>
+                        Please fill your information in the fields below:
+                    </DialogContentText>
+                    {isRegister && (
+                        <TextField
+                            autoFocus
+                            margin="normal"
+                            variant="standard"
+                            id="name"
+                            label="Name"
+                            type="text"
+                            fullWidth
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            inputProps={{ minLength: 2 }}
+                            inputRef={nameRef}
+                            required
+                        />
+                    )}
+                    <TextField
+                        autoFocus={!isRegister}
+                        margin="normal"
+                        variant="standard"
+                        id="email"
+                        label="Email"
+                        type="text"
+                        fullWidth
+                        value={email}
+                        inputRef={emailRef}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <PasswordField
+                        id="password"
+                        label="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    {isRegister && (
+                        <PasswordField
+                            id="confirmPassword"
+                            label="Confirm Password"
+                            passwordRef={confirmPassword}
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    )}
+                </DialogContent>
+                <DialogActions>
+                    <Button type="submit" variant="contained" endIcon={<Send />}>
+                        Submit
+                    </Button>
+                </DialogActions>
+            </form>
+            <DialogActions sx={{ justifyContent: 'left', p: '5px 24px' }}>
+                {isRegister ? 'Do you have an account? ' : 'Don\'t have an account? '}
+                <Button onClick={() => setIsRegister(!isRegister)}>
+                    {isRegister ? 'Sign in now' : 'Sign up now'}
                 </Button>
             </DialogActions>
-        </form>
-        <DialogActions sx={{justifyContent:'left', p:'5px 24px'}}>
-           {isRegister?'Do you have an account? ' : 'Don\'t have an account? '} 
-           <Button onClick={()=>setIsRegister(!isRegister)}>
-            {isRegister ? 'Sign in now' : 'Sign up now'}
-           </Button>
-        </DialogActions>
-        <DialogActions sx={{ justifyContent: 'center', py:'24px'}}>
-            <GoogleOneTapLogin />
-        </DialogActions>
-    </Dialog>
-  )
-}
+            <DialogActions sx={{ justifyContent: 'center', py: '24px' }}>
+                <GoogleOneTapLogin />
+            </DialogActions>
+        </Dialog>
+    );
+};
 
-export default Login
+export default Login;
--- client/src/components/user/PasswordField.js
+++ client/src/components/user/PasswordField.js
@@ -1,45 +1,46 @@
-import { IconButton, InputAdornment, TextField } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { IconButton, InputAdornment, TextField } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
-const PasswordField = ({passwordRef, id='password', label='Password'}) => {
-
-    const [showPassword, setShowPassword] = useState(false)
+const PasswordField = ({ id = 'password', label = 'Password', value, onChange }) => {
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleClick = () => {
-      setShowPassword(!showPassword)
-    }
+        setShowPassword(!showPassword);
+    };
 
     const handleMouseDown = (e) => {
-      e.preventDefault()
-    }
+        e.preventDefault();
+    };
 
-  return (
-    <TextField
-    autoFocus
-    margin='normal'
-    variant='standard'
-    id={id}
-    label={label}
-    type={showPassword?'text':'password'}
-    fullWidth
-    inputRef={passwordRef}
-    inputProps={{minLength:6}}
-    required
-    InputProps={{
-      endAdornment: (
-          <InputAdornment position='end'>
-              <IconButton
-                  onClick={handleClick}
-                  onMouseDown={handleMouseDown}
-              >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-          </InputAdornment>
-      ),
-  }}  
-    />
-  )
-}
+    return (
+        <TextField
+            autoFocus
+            margin='normal'
+            variant='standard'
+            id={id}
+            label={label}
+            type={showPassword ? 'text' : 'password'}
+            fullWidth
+            inputProps={{ minLength: 6 }}
+            required
+            autoComplete='on'
+            value={value}
+            onChange={onChange}
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment position='end'>
+                        <IconButton
+                            onClick={handleClick}
+                            onMouseDown={handleMouseDown}
+                        >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                ),
+            }}
+        />
+    );
+};
 
-export default PasswordField
+export default PasswordField;
