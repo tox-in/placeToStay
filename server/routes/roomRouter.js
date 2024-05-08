b29bd3444d9bc0c81845f5e96cb6d225bf335a58
tox-in <habimanat78@gmail.com>
2024-05-08 14:16:04 +0200

    included the auth

--- server/routes/roomRouter.js
+++ server/routes/roomRouter.js
@@ -1,7 +1,8 @@
 import {Router} from 'express'
 
 import { createRoom } from '../controllers/room.js';
+import auth from '../middleware/auth.js'
 
 const roomRouter = Router();
-roomRouter.post('/',createRoom);
+roomRouter.post('/',auth,createRoom);
 export default roomRouter;
