bbb1aba2228a590a3ca064e780ef79b4f09b0880
tox-in <habimanat78@gmail.com>
2024-05-08 12:18:42 +0200

    testing the backend authorization

--- client/src/components/user/UserMenu.js
+++ client/src/components/user/UserMenu.js
@@ -5,10 +5,36 @@ import { useValue } from '../../context/contextProvider';
 
 const UserMenu = ({ anchorUserMenu, setAnchorUserMenu }) => {
 
-    const {dispatch} = useValue()
+    const {
+        dispatch,
+        state: { currentUser },
+        } = useValue();
 
     const handleCloseUserMenu = () => {
         setAnchorUserMenu(null)
+    }
+
+    const testAuthorization = async()=>{
+        const url = process.env.REACT_APP_SERVER_URL + '/room'
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: `Bearer ${currentUser.token}`
+                }
+            })
+            const data = await response.json()
+
+            console.log(data)
+
+            if(!data.success){
+                throw new Error(data.message)
+            }
+        } catch(error) {
+            dispatch({type:'UPDATE_ALERT', payload:{open:true, severity:'error', message:error.message}})
+            console.log(error)
+        }
     }
 
     return (
@@ -18,7 +44,7 @@ const UserMenu = ({ anchorUserMenu, setAnchorUserMenu }) => {
             onClose={handleCloseUserMenu}
             onClick={handleCloseUserMenu}
         >
-            <MenuItem>
+            <MenuItem onClick={testAuthorization}>
                 <ListItemIcon>
                     <Settings fontSize='small' />
                 </ListItemIcon>
