1af64540330566425c6811d3b50c57ebedbf580f
tox-in <habimanat78@gmail.com>
2024-05-08 12:20:47 +0200

    working on the running server and frontend integration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import roomRouter from './routes/roomRouter.js';
+import cors from 'cors';
 
 dotenv.config()
+
+const app = express();
 
 const port = process.env.PORT || 5000
 
@@ -12,8 +16,16 @@ app.use((req,res) =>{
     next()
 })
 
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['X-Requested-With', 'Content-Type', 'Authorization']
+}));
+
+
 app.use(express.json({limit:'10mb'}))
 
+app.use('/room', roomRouter);
 app.use('/', (req,res)=>res.json({message:'Welcome to our API'}))
 app.use((req,res)=>res.status(404).json({success:false, message:'Not Found'}))
 
