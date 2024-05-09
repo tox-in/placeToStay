5b4374db821a373092241bba356ef60aa4ef08a6
tox-in <habimanat78@gmail.com>
2024-05-09 08:13:28 +0200

    defined the User entity model

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,11 @@
+import mongoose from "mongoose"
+
 const userSchema = mongoose.Schema({
     name:{type:String, min:2, max:50, required},
-    email:{type:String, min:2, max:50, required, unique:true
+    email:{type:String, min:5, max:50, required, unique:true},
+    password:{type:String, required},
+    photoUrl:{type:String, default:''}
 })
+
+const User = mongoose.model('users',userSchema);
+export default User;
