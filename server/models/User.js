76c4462339d9fb19f06e14cad0d142ec7ebcde77
tox-in <habimanat78@gmail.com>
2024-05-09 21:59:44 +0200

    changes in User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose"
 
 const userSchema = mongoose.Schema({
-    name:{type:String, min:2, max:50, required},
-    email:{type:String, min:5, max:50, required, unique:true},
-    password:{type:String, required},
+    name:{type:String, min:2, max:50, required:true},
+    email:{type:String, min:5, max:50, required:true, unique:true},
+    password:{type:String, required:true},
     photoUrl:{type:String, default:''}
 })
 
